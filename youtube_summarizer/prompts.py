bde256634d6f3874323334421e14a58c816e03d5
Alexander Slivinskiy <alexander.slivinskiy@gmail.com>
2025-07-28 23:29:46 +0200

    renamed prompt to summary_prompt

--- youtube_summarizer/prompts.py
+++ youtube_summarizer/prompts.py
@@ -1,4 +1,4 @@
-prompt = """
+summary_prompt = """
     Summarize the following youtube transcript.
     It's an automatically generated transcript of a video, so it may contains errors in grammar, spelling and punctuation.
     Create bullet points and use markdown formatting.
